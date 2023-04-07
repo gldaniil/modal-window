a14b515e875fb672cca48dfdc7be88fbf81636ee
gldaniil <daniil.gajfullin@yandex.ru>
2023-04-07 19:03:13 +0500

    Улучшенная маска

--- index.js
+++ index.js
@@ -1,120 +1,174 @@
-// Открыть модальное окно при клике на кнопку
-document.getElementById("myBtn").addEventListener("click", function() {
-  document.querySelector(".modal").style.display = "block";
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const phoneInputs = document.querySelectorAll("#phone");
+  const nameInputs = document.querySelectorAll("#name");
+  const submitBtns = document.querySelectorAll("#btn-submit");
 
-// Закрыть модальное окно при клике на крестик или за его пределами
-document.querySelectorAll(".close, .modal").forEach(function(elem) {
-  elem.addEventListener("click", function() {
-    document.querySelector(".modal").style.display = "none";
-    nameInput.value = "";
-    phoneInput.value = ""; // Очистка поля ввода
+    // Открыть модальное окно при клике на кнопку
+  document.getElementById("myBtn").addEventListener("click", function() {
+    document.querySelector(".modal").style.display = "block";
   });
-});
 
-// Предотвращение закрытия модального окна при клике внутри него
-document.querySelector(".modal-content").addEventListener("click", function(event) {
-  event.stopPropagation();
-});
+  // Закрыть модальное окно при клике на крестик или за его пределами
+  document.querySelectorAll(".close, .modal").forEach(function(elem) {
+    elem.addEventListener("click", function() {
+      document.querySelector(".modal").style.display = "none";
+      [...nameInputs].forEach((el) => { 
+        el.value = "", 
+        el.className = "",
+        el.nextElementSibling.innerHTML = ""
+      });
+      [...phoneInputs].forEach((el) => { 
+        el.value = "", 
+        el.className = "",
+        el.nextElementSibling.innerHTML = ""
+      }); // Очистка поля ввода
+    });
+  });
 
-const phoneInput = document.querySelector("#phone");
-const nameInput = document.querySelector("#name");
-const submitBtn = document.querySelector("#btn-submit");
+  // Предотвращение закрытия модального окна при клике внутри него
+  document.querySelector(".modal-content").addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
 
-let keyCode;
-function maskedPhone(e) {
-  e.keyCode && (keyCode = e.keyCode);
-  let pos = this.selectionStart;
-  if (pos < 3) e.preventDefault();
-  let matrix = "+7 (___)-___-__-__",
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, ""),
-    newVal = matrix.replace(/[_\d]/g, (a) => ( i < val.length ? val.charAt(i++) || def.charAt(i) : a))
-  i = newVal.indexOf("_");
-  if (i != -1) i < 5 && (i = 3);
-  let reg = matrix.slice(0, this.value.length).replace(/_+/g, (a) => {
-    return `\\d{1,${a.length}}`}).replace(/[+()]/g, "\\$&");
-   reg = new RegExp(`^${reg}$`);
-  if (!reg.test(this.value) || this.value.length < 5) this.value = newVal;
-
-  if (e.keyCode === 8 || e.keyCode === 46 || e.keyCode === 151 && this.value.length > 0) {
-    // удаляем значение
-    this.value = this.value.replace(/\d(?=[^\d]*$)/, '_');
-  }
-
-  if (newVal.indexOf("_") != -1) {
-    this.setSelectionRange(newVal.indexOf("_"), newVal.indexOf("_"));
-  } else {
-    this.setSelectionRange(this.value.length, this.value.length);
-  }
-}
-// Пол
-function maskedPhoneValidation(e) {
-  let counter = 0;
-  for (let val of this.value) {
-    if (!isNaN(parseInt(val))) {
-      counter++;
+  // Пол
+  function maskedPhoneValidation(e) {
+    let counter = 0;
+    for (let val of this.value) {
+      if (!isNaN(parseInt(val))) {
+        counter++;
+      }
+    }
+    if (counter != 11) {
+      this.value = ""
+      addBlockMessage(this, true)
+    } else {
+      addBlockMessage(this, false)
     }
   }
-  if (counter != 11) {
-    this.value = ""
-    addBlockMessage(this, true)
-  } else {
-    addBlockMessage(this, false)
-  }
-}
 
-const addBlockMessage = (e, b = true) => {
-  if (b) {
-    if (e.classList.contains('is-valid')) {
-      e.classList.remove('is-valid')
+  const addBlockMessage = (e, b = true) => {
+    if (b) {
+      if (e.classList.contains('is-valid')) {
+        e.classList.remove('is-valid')
+      }
+      e.classList.add('is-invalid')
+      e.nextElementSibling.innerHTML = "Необходимо заполнить поле"
+    } else {
+      if (e.classList.contains('is-invalid')) {
+        e.classList.remove('is-invalid')
+      }
+      e.classList.add("is-valid")
+      e.nextElementSibling.innerHTML = ""
     }
-    e.classList.add('is-invalid')
-    e.nextElementSibling.innerHTML = "Необходимо заполнить поле"
-  } else {
-    if (e.classList.contains('is-invalid')) {
-      e.classList.remove('is-invalid')
+  }
+
+  function nameValidation(e) {
+    if (this.value.length < 2) {
+      this.value = ""
+      addBlockMessage(this, true)
+    } else {
+      addBlockMessage(this, false)
     }
-    e.classList.add("is-valid")
-    e.nextElementSibling.innerHTML = ""
   }
-}
 
-function nameValidation(e) {
-  if (this.value.length < 2) {
-    this.value = ""
-    addBlockMessage(this, true)
-  } else {
-    addBlockMessage(this, false)
+  function maskedName(e) {
+    const pattern = /^[a-zA-Zа-яА-Я]+$/; // регулярное выражение для букв
+    if (!pattern.test(this.value)) {
+      this.value = this.value.replace(/[^a-zA-Zа-яА-Я]/g, ""); // удаляем все неправильные символы
+    }
+  };
+
+  let getInputNumbersValue = function (input) {
+      // Return stripped input value — just numbers
+      return input.value.replace(/\D/g, '');
   }
-}
 
-function maskedName(e) {
-  const pattern = /^[a-zA-Zа-яА-Я]+$/; // регулярное выражение для букв
-  if (!pattern.test(this.value)) {
-    this.value = this.value.replace(/[^a-zA-Zа-яА-Я]/g, ""); // удаляем все неправильные символы
+  let onPhonePaste = function (e) {
+      let input = e.target,
+          inputNumbersValue = getInputNumbersValue(input);
+      let pasted = e.clipboardData || window.clipboardData;
+      if (pasted) {
+          let pastedText = pasted.getData('Text');
+          if (/\D/g.test(pastedText)) {
+              // Attempt to paste non-numeric symbol — remove all non-numeric symbols,
+              // formatting will be in onPhoneInput handler
+              input.value = inputNumbersValue;
+              return;
+          }
+      }
   }
-};
 
+  let onPhoneInput = function (e) {
+      let input = e.target,
+          inputNumbersValue = getInputNumbersValue(input),
+          selectionStart = input.selectionStart,
+          formattedInputValue = "";
 
+      if (!inputNumbersValue) {
+          return input.value = "";
+      }
 
-phoneInput.addEventListener("input", maskedPhone)
-phoneInput.addEventListener("focus", maskedPhone)
-phoneInput.addEventListener("blur", maskedPhoneValidation)
-phoneInput.addEventListener("keydown", maskedPhone)
+      if (input.value.length != selectionStart) {
+          // Editing in the middle of input, not last symbol
+          if (e.data && /\D/g.test(e.data)) {
+              // Attempt to input non-numeric symbol
+              input.value = inputNumbersValue;
+          }
+          return;
+      }
 
-nameInput.addEventListener("input", maskedName)
-nameInput.addEventListener("blur", nameValidation)
-
-submitBtn.addEventListener("click", function(e) {
-  e.preventDefault();
-  for (let el of [nameInput, phoneInput]) {
-    if (el.value === '') {
-      // Если поля ввода пустые
-      addBlockMessage(el)
-      console.log(el) 
-    } else { }
+      if (["0", "1", "2", "3", "4", "5", "6" ,"7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+          if (inputNumbersValue[0] != "7") inputNumbersValue = "7" + inputNumbersValue;
+          let firstSymbols = "+7"
+          formattedInputValue = input.value = firstSymbols + " ";
+          if (inputNumbersValue.length > 0) {
+              formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+          }
+          if (inputNumbersValue.length >= 5) {
+              formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+          }
+          if (inputNumbersValue.length >= 8) {
+              formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+          }
+          if (inputNumbersValue.length >= 10) {
+              formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+          }
+      } else {
+          formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+      }
+      console.log(formattedInputValue)
+      input.value = formattedInputValue;
   }
-  // console.log(phoneInput.value)
+  let onPhoneKeyDown = function (e) {
+      // Clear input after remove last symbol
+      let inputValue = e.target.value.replace(/\D/g, '');
+      if (e.keyCode == 8 && inputValue.length == 1) {
+          e.target.value = "";
+      }
+  }
+  for (let phoneInput of phoneInputs) {
+      phoneInput.addEventListener('keydown', onPhoneKeyDown);
+      phoneInput.addEventListener('input', onPhoneInput);
+      phoneInput.addEventListener('paste', onPhonePaste);
+      phoneInput.addEventListener('blur', maskedPhoneValidation)
+  }
+
+  for (let nameInput of nameInputs) {
+    nameInput.addEventListener("input", maskedName)
+    nameInput.addEventListener("blur", nameValidation)
+  }
+
+  for (let submitBtn of submitBtns) {
+    submitBtn.addEventListener("click", function(e) {
+      e.preventDefault();
+      for (let el of [...nameInputs, ...phoneInputs]) {
+        if (el.value === '') {
+          // Если поля ввода пустые
+          addBlockMessage(el)
+          console.log(el) 
+        } else { }
+      }
+      // console.log(phoneInput.value)
+    })
+  }
 })
